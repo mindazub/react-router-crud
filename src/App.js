f3fd8e8768e282e829734c47785080d8b3f29455
Mindaugas Azubalis <mindaugas.azubalis@gmail.com>
2020-02-04 18:12:03 +0200

    not finished but meh, fuck it

--- src/App.js
+++ src/App.js
@@ -1,74 +1,42 @@
 import React, { Component } from "react";
-import Child from "./components/parentToChild/Child";
 
-class App extends Component {
+type TestProps = {
+  str: string,
+  num?: number,
+  ary: Array<number>
+};
+
+type TestState = {
+  message: string
+};
+
+class Test extends Component<TestProps> {
   state = {
-    name: "Peter"
+    message: "hello"
   };
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "John"
-    };
-    console.log("constructor");
-  }
-
-  componentWillMount() {
-    if (window.innerWidth < 500) {
-      this.setState({ innerWidth: window.innerWidth });
-    }
-
-    console.log("Component will mount");
-  }
-
-  componentDidMount() {
-    console.log("component did mount");
-  }
-
-  componentWillReceiveProps() {
-    console.log("componentWillReceiveProps");
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log("shouldComponentUpdate");
-    return true;
-  }
-
-  componentWillUpdate() {
-    console.log("componentWillUpdate, no to setState here!");
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log("CHILD prevProps", prevProps);
-    console.log("CHILD prevState", prevState);
-    console.log("CHILD componentDidUpdate");
-  }
-
-  componentWillUnmount() {
-    console.log("componentWillUnmount");
-  }
-
-  changeState() {
-    this.setState({ name: "Joe" });
-  }
-
-  unmountChild(){
-    this.setState({name: 'Robert Unmount'});
-  }
+  static defaultProps = {
+    str: "mindaugas"
+  };
+  p:? HTMLBodyElement;
 
   render() {
-    console.log("render");
-    if(this.state.name === 'Robert Unmount'){
-      return(<div/>);
-    }
+    return (
+      <div className="Test">
+        <p>{this.props.str}</p>
+        <p>{this.props.num}</p>
+        <p>{this.props.ary.map(val => val)}</p>
+        <p ref={p => (this.p = p)}>{this.state.message}</p>
+      </div>
+    );
+  }
+}
+
+class App extends Component<{}> {
+  render() {
     return (
       <div className="App">
-        name: {this.state.name} | innerWIdth: {this.state.innerWidth}
-        <Child name={this.state.name} />
-        <button onClick={this.changeState.bind(this)}>Change State</button>
-
-        <button onClick={this.unmountChild.bind(this)}>Unmount CHILD</button>
+        <Test ary={[1, 2, 3]} />
       </div>
     );
   }
